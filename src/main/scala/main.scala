5fe63ba571900a02e96779c2e5f59d59cae94138
Jaceguai De Magalhaes <jaceguai@gmail.com>
2022-04-04 15:41:51 -0400

    Delete main.scala

--- src/main/scala/main.scala
+++
@@ -1,9 +0,0 @@
-import system.Logging
-
-object main extends App{
-  var UN = "Test main 1"
-
-  val logging = new Logging()
-  logging.InsertLog("log from main 2",UN, this.getClass.getSimpleName.toLowerCase())
-  logging.ListLog()
-}
